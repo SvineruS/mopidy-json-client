cf00d1ed6d9a07289b8a566086733ecf7bdda6f2
svinerus <svinerus@gmail.com>
2020-12-21 08:50:27 +0200

    from scratch

--- generate_api.py
+++ generate_api.py
@@ -1,67 +1,72 @@
 #!/usr/bin/python
-import sys
-from mopidy_json_client import SimpleClient
+import asyncio
+from collections import defaultdict
+from pathlib import Path
+
+from mopidy_async_client import MopidyClient
+
+_BASE = """
+class _BaseController:
+    def __init__(self, request_handler):
+        self._request_handler_ = request_handler
+
+    async def mopidy_request(self, method, **kwargs):
+        return await self._request_handler_(method, **kwargs)
+
+"""
 
 
-def controller_name(mopidy_module):
-    '''Returns CamelCase name for the controller class of a given mopidy_module'''
-    return mopidy_module[0].upper() + mopidy_module[1:] + 'Controller'
+def get_controllers(description):
+    def get_controller_code():
+        functions_ = "\n".join(functions)
+        return f'''
+class {controller_name.capitalize()}Controller(_BaseController):
+{functions_}
+
+    '''
+
+    controllers = defaultdict(list)
+
+    for endpoint_name, method_info in description.items():
+        *_, controller_name, method_name = endpoint_name.split('.')
+        func_code = get_func_code(endpoint_name, method_name, method_info)
+        controllers[controller_name].append(func_code)
+
+    result = ""
+    for controller_name, functions in controllers.items():
+        result += get_controller_code()
+    return result
 
 
-def generate_controller_code(mopidy_module, methods, version, exclude_parms={'kwargs'}):
+def get_func_code(endpoint_name, method_name, method_info):
+    def get_arg(param):
+        return ("{name}={default}".format(**param) if 'default' in param else param['name']) + ', '
 
-    def nl(indent):
-        tab_spaces = 4
-        return '\n' + ' ' * indent * tab_spaces
+    def get_usage(param):
+        return "{name}={name}, ".format(**param)
 
-    print('from . import MopidyWSController\n')
-    print('class %s(MopidyWSController):' % controller_name(mopidy_module))
-    print("%s''' Auto-generated %s Class for Mopidy JSON/RPC API version %s'''\n" % (nl(1),
-                                                                                     controller_name(mopidy_module),
-                                                                                     version))
+    docs = method_info['description'].replace("\n\n", "\n").replace("\n", "\n        ")
+    args = "".join([get_arg(p) for p in method_info['params']])
+    usage = "".join([get_usage(p) for p in method_info['params']])
+    deprecated = "\n    # DEPRECATED" if ".. deprecated::" in docs else ""
 
-    for method, info in methods.items():
-        # Method name
-        method_name = method.split('.')[2]
-        # Parameters
-        params_def = ''
-        params_use = ''
-        for param in info.get('params'):
-            if param['name'] not in exclude_parms:
-                params_def += ('{name}={default}'.format(param) if 'default' in param else param['name']) + ', '
-                params_use += '{name}={name}'.format(param) + ', '
+    return f'''{deprecated}
+    async def {method_name}(self, {args}**options):
+        """{docs}"""
+        return await self.mopidy_request('{endpoint_name}', {usage}**options)'''
 
-        # Generate function parts
-        _definition = "def %s(self, %s**options):" % (method_name, params_def)
-        _body = "return self.mopidy_request('%s', %s**options)" % (method, params_use)
-        _description = "'''" + info.get('description').replace('\n\n', '\n').replace('\n', nl(2)) + nl(2) + "'''"
-        _comments = (nl(1) + '# DEPRECATED') if '.. deprecated::' in _description else ''
 
-        _function = _comments + nl(1) + _definition + nl(2) + _description + nl(2) + _body
+async def main():
+    async with MopidyClient() as client:
+        description = await client.core.describe()
 
-        # Print Wrapper Function for the method
-        print(_function)
+    description['core.describe'] = {'description': "Get all endpoints", 'params': []}
+
+    code = _BASE + get_controllers(description)
+    path = Path(__file__).parent / "mopidy_async_client" / "mopidy_api.py"
+    with open(path, 'w') as file:
+        file.write(code)
 
 
 if __name__ == '__main__':
-
-    mopidy_modules = {'playback',
-                      'mixer',
-                      'tracklist',
-                      'library',
-                      'playlists',
-                      'history'}
-
-    if len(sys.argv) == 2:
-        mopidy_modules = {sys.argv[1]}
-    elif len(sys.argv) > 2:
-        print 'Usage: generate_api.py [mopidy_module]'
-        exit(1)
-
-    client = SimpleClient()
-    version = client.core.get_version()
-    api = client.core.describe()
-
-    for module in mopidy_modules:
-        methods = {method: info for method, info in api.items() if method.startswith('core.' + module)}
-        generate_controller_code(module, methods, version=version)
+    asyncio.run(main())
---
+++ mopidy_async_client/__init__.py
@@ -0,0 +1,9 @@
+# Fork of https://github.com/ismailof/mopidy-json-client by ismailof
+__author__ = 'svinerus (svinerus@gmail.com)'
+__version__ = '0.1'
+
+from .client import MopidyClient
+
+__all__ = [
+    'MopidyClient',
+]
